2a5acbc2b477c55199dd08b56a3eead700d4fc6e
José Alejandro Arellano Munguía <jarellanom1500@alumno.ipn.mx>
2024-02-22 18:29:59 -0600

    Se agregó lógica de la petición PUT.

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -35,4 +35,13 @@ public class Medico {
         this.direccion = new Direccion(datosRegistroMedico.direccion());
         this.telefono = datosRegistroMedico.telefono();
     }
+
+    public void actualizarDatos(DatosActualizarMedico datosActualizarMedico) {
+        if (datosActualizarMedico.nombre() != null)
+            this.nombre = datosActualizarMedico.nombre();
+        if (datosActualizarMedico.documento() != null)
+            this.documento = datosActualizarMedico.documento();
+        if (datosActualizarMedico.direccion() != null)
+            this.direccion = direccion.actualizarDatos(datosActualizarMedico.direccion());
+    }
 }
